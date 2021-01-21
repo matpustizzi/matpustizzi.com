3ad0a2aa889f0a86ef3e1d1c0f1e85ad9239862e
Mat Pustizzi <mathew.pustizzi@gmail.com>
2021-01-21 15:13:09 -0500

    initial commit

---
+++ _data/contentful.js
@@ -0,0 +1,27 @@
+const contentful = require("contentful");
+const client = contentful.createClient({
+  space: process.env.CTFL_SPACE,
+  accessToken: process.env.CTFL_ACCESSTOKEN
+});
+
+module.exports = async function() {
+    const entries = await client.getEntries({ content_type: 'page', order: 'sys.createdAt' })        
+        .then(function(response) {
+            const page = response.items
+            .map(function(page) {
+                page.fields.date= new Date(page.sys.updatedAt);
+                return page.fields;
+            });
+            return page;
+        })   
+        .catch(console.error);
+    
+    
+    const data = {
+        pages : entries.filter((entry) => entry.slug != 'home'),
+        home: entries.filter((entry) => entry.slug === 'home')
+    }
+
+    console.log(data);
+    return data;
+}
