4fb553f9c5ac619f2a2f4bd3569e9ab6c0f106e1
Mat Pustizzi <mathew.pustizzi@gmail.com>
2021-01-24 18:21:31 -0500

    styling

--- _data/globals.js
+++ _data/globals.js
@@ -1,3 +1,3 @@
 module.exports = {
-    siteName : "Mathew Pustizzi"
+    siteName : "Mat Pustizzi"
 }
--- css/styles.11ty.js
+++ css/styles.11ty.js
@@ -1,10 +1,21 @@
 const fs = require('fs');
 const path = require('path');
 const postcss = require('postcss');
-
 // the file name as an entry point for postcss compilation
 // also used to define the output filename in our output /css folder.
 const fileName = "styles.css";
+const rfsOptions = {
+  twoDimensional: false,
+  baseValue: 14,
+  unit: 'rem',
+  breakpoint: 1200,
+  breakpointUnit: 'px',
+  factor: 10,
+  class: false,
+  unitPrecision: 6,
+  safariIframeResizeBugFix: false,
+  remValue: 16
+};
 
 module.exports = class {
   async data () {
@@ -21,6 +32,8 @@ module.exports = class {
       require('precss'),
       require('postcss-import'),
       require('postcss-mixins'),
+      require('rfs/postcss')(rfsOptions),
+      require('postcss-nesting'),
       require('cssnano')
     ])
     .process(rawCss, { from: rawFilepath })
